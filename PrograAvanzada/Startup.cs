cebde9d0d60af7beaa457c3b8ed755422ae78fa0
Jesus <jesus.rojas.umana@gmail.com>
2016-08-03 19:55:10 -0600

    Add project files.

---
+++ PrograAvanzada/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrograAvanzada.Startup))]
+namespace PrograAvanzada
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
